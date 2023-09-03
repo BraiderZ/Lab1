4d1349c4e8af4e3f8ad2f8bd410525d2b710a05a
BraiderZ <luisjbc2112@gmail.com>
2023-09-03 15:04:04 -0600

    Nuevo if para verificar si existe el archivo con mensaje de error si no existe

--- script1.sh
+++ script1.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 if [ $# -lt 1 ]; then
-	echo "Error: agregue un archivo como argumento"
+	echo "Error: agregue un archivo como argumento."
 	exit 1
 elif [ $# -gt 1 ]; then
-        echo "Error: solo puede agregar un archivo a la vez"
+        echo "Error: solo puede agregar un archivo a la vez."
         exit 1
 fi
-	
+
+if [ ! -e $1 ]; then
+	echo "Error: el archivo $1 no existe."
+	exit 1
+fi
